c05f39593c0fd694a2613a6fa2ded40f82e80eaa
rlqha <rlqha123@gmail.com>
2016-04-08 16:19:34 +0900

    초기값

---
+++ Classes/MainScene.cpp
@@ -0,0 +1,43 @@
+﻿#include "MainScene.h"
+#include "SimpleAudioEngine.h"
+
+using namespace cocos2d;
+using namespace CocosDenshion;
+
+//android effect only support ogg
+#if(CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+#define EFFECT_FILE		"Sounds/effect2.ogg"
+#elif(CC_TARGET_PLATFORM == CC_PLATFORM_MARMALADE)
+#define EFFECT_FILE		"Sounds/effect1.raw"
+#else
+#define EFFECT_FILE		"Sounds/effect1.wav"
+#endif // CC_PLATFORM_ANDROID
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+#define MUSIC_FILE		"Sounds/music.mid"
+#elif(CC_TARGET_PLATFORM == CC_PLATFORM_BLACKBERRY)
+#define MUSIC_FILE		"Sounds/background.ogg"
+#else
+#define MUSIC_FILE		"Sounds/background.mp3"
+#endif // CC_PLATFORM_WIN32
+
+Scene* Main::createScene()
+{
+	auto scene = Scene::create();
+	auto layer = Main::create();
+	scene->addChild(layer);
+
+	return scene;
+}
+
+bool Main::init()
+{
+	if (!LayerColor::initWithColor(Color4B(255, 255, 255, 255)))
+	{
+		return false;
+	}
+
+	/////////////////////////////
+
+	return true;
+}
---
+++ Classes/MainScene.h
@@ -0,0 +1,26 @@
+﻿#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class Main : public cocos2d::LayerColor
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+    CREATE_FUNC(Main);
+
+
+	//Menu
+	cocos2d::Sprite* pMenuItem1;
+	cocos2d::Sprite* pMenuItem2;
+
+
+	//Sound
+	unsigned int m_nSoundId;
+	void doSoundAction(Ref* pSender);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
